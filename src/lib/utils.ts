2e43ff28ba7e1b86df480b23a5f309fda3ab7614
PRABHAT RAJ <kp18fb@gmail.com>
2025-06-29 15:42:16 +0530

    url fix

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -10,7 +10,7 @@ export function absoluteUrl(path: string) {
   if (typeof window !== 'undefined') return path
   if (process.env.VERCEL_URL)
     return `https://${process.env.VERCEL_URL}${path}`
-  return `http://localhost:${process.env.PORT ?? 3000}${path}`
+  return `https://lexinote.vercel.app'}${path}`
     
 }
 
