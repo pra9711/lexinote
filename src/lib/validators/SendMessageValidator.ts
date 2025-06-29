2be83934ee078abf7ac7916fa5b5b605ce5376e7
PRABHAT RAJ <kp18fb@gmail.com>
2025-06-29 15:20:17 +0530

    Revert "Fix API route and secure environment variables"
    This reverts commit e250c7da40bd58732c52f6860fea1e8a9252e824.

--- src/lib/validators/SendMessageValidator.ts
+++ src/lib/validators/SendMessageValidator.ts
@@ -2,7 +2,7 @@ import { z } from 'zod'
 
 export const SendMessageValidator = z.object({
   fileId: z.string(),
-  message: z.string().min(1).max(2000)
+  message: z.string()
 })
 
 export type SendMessageRequest = z.infer<typeof SendMessageValidator>
