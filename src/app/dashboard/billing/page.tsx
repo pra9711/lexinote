be6e967e5d030cf265b6d612240a4e78792cd70a
PRABHAT RAJ <kp18fb@gmail.com>
2025-06-29 23:55:20 +0530

    day

--- src/app/dashboard/billing/page.tsx
+++ src/app/dashboard/billing/page.tsx
@@ -1,16 +1,7 @@
 import BillingForm from "@/components/BillingForm";
 import { getUserSubscriptionPlan } from "@/lib/stripe";
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import { redirect } from "next/navigation";
 
 const Page = async () => {
-  const { getUser } = getKindeServerSession();
-  const user = await getUser();
-
-  if (!user || !user.id) {
-    redirect("/auth-callback?origin=dashboard/billing");
-  }
-
   const subscriptionPlan = await getUserSubscriptionPlan();
 
   return <BillingForm subscriptionPlan={subscriptionPlan} />;
