2487106f8914cf4296b4911cdaf5a6976ac55d41
PRABHAT RAJ <kp18fb@gmail.com>
2025-08-08 23:08:28 +0530

    bugsolve

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = constructMetadata();
+export const dynamic = 'force-dynamic';
 
 export default function RootLayout({
   children,
